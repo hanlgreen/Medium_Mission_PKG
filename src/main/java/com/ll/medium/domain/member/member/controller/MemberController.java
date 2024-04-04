85a3d1e0f297414e2857e72785f2c5aa84f49b94
luckygoldstar <luckygoldsta@naver.com>
2024-04-04 13:58:58 +0900

    work

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -30,9 +31,12 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String signup(@Valid JoinForm joinForm) {
-        memberService.join(joinForm.getUsername(), joinForm.getPassword());
+    public String join(@Valid JoinForm joinForm) {
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
-        return "redirect:/";
+        long id = member.getId();
+
+        return "redirect:/?msg=No %d member joined.".formatted(id);
+
     }
 }
